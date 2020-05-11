ab5dd33c81c5296a9c377379967dc3f2bb6b8bd7
SammyJoskey <55281833+SammyJoskey@users.noreply.github.com>
2020-05-11 22:27:52 +1000

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from celery import Celery
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CsrfProtect
+from app import routes, models, forms
+from .config import Config
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    app.debug = True
+    return app
+
+app = create_app()
+db = SQLAlchemy(app)
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+
+db.create_all()
