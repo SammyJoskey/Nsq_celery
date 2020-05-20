e30e84226a99e7efe9013546fbd1b43360bf8d82
SammyJoskey <55281833+SammyJoskey@users.noreply.github.com>
2020-05-20 10:55:24 +1000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,18 +3,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CsrfProtect
 
-from config import Config
+
+from config import Configuration
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(Configuration)
     app.debug = True
     return app
 
 app = create_app()
 db = SQLAlchemy(app)
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery = Celery(app.name, broker='redis://localhost:6379/0')
 celery.conf.update(app.config)
 
 from app import routes, models, forms
